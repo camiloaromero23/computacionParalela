b7c315c20eca3113761e051e744f8206bc9aa392
Camilo Romero <camiloaromero23@gmail.com>
2021-05-03 19:18:54 -0500

    Changed libraries imports and functions names

--- tercerCorte/taller1/test.py
+++ tercerCorte/taller1/test.py
@@ -1,5 +1,5 @@
-from functionE import rbf_network
-from cy_functionE import cy_rbf_network
+import functionE
+import cy_functionE
 import numpy as np
 import time
 
@@ -10,13 +10,13 @@ beta = np.random.rand(N)
 theta = 10
 
 start = time.time()
-rbf_network(X, beta, theta)
+functionE.rbf_network(X, beta, theta)
 
 
 total_time = time.time() - start
 
 start = time.time()
-cy_rbf_network(X, beta, theta)
+cy_functionE.rbf_network(X, beta, theta)
 cy_total_time = time.time() - start
 
 speedUp = round(total_time/cy_total_time, 3)
