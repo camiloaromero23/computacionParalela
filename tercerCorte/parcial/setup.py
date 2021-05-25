a602448c722e708d607d377f3d00874d5bed17ad
Camilo Romero <camiloaromero23@gmail.com>
2021-05-24 19:31:35 -0500

    Fixed setup file for correctly using numpy

--- tercerCorte/parcial/setup.py
+++ tercerCorte/parcial/setup.py
@@ -1,27 +1,10 @@
-from Cython.Distutils import build_ext
-from distutils.extension import Extension
+from setuptools import setup
 from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
 
-exts = (cythonize('cy_heat.pyx', annotate=True))
+exts = (cythonize('cy_heat.pyx', language_level="3", annotate=True))
 
-setup(ext_modules=exts)
-
-
-ext_modules = [
-    Extension('cy_heat',
-              ['cy_heat.pyx'],
-              libraries=['m'],
-              extra_compile_args=['-ffast-math',
-                                  '-fopenmp', '-march=native'],
-              extra_link_args=['-fopenmp'],
-              include_dirs=[numpy.get_include()],
-              annotate=True
-              )]
-
-setup(
-    name='cy_heat',
-    cmdclass={'build_ext': build_ext},
-    ext_modules=ext_modules
-)
+setup(ext_modules=exts,
+      include_dirs=[numpy.get_include()],
+      )
